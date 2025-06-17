72fba96d121f38f2cec667670b615f263e701f82
Rodrigo Araujo <rasch.dev@gmail.com>
2025-06-17 03:50:31 -0300

    Created Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,10 @@
 package application;
 
-public class Main {
+import boardgame.Position;
 
+public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Position pos = new Position(1, 2);
 
 	}
-
 }
