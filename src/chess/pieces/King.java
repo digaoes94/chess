efb2c1174d8413de4dac30974d91daf36f00f077
Rodrigo Araujo <rasch.dev@gmail.com>
2025-06-30 18:57:57 -0300

    Created King::canMove. Updated King::possibleMoves.

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,9 +16,64 @@ public class King extends ChessPiece {
 		return "K";
 	}
 
+	private boolean canMove(Position pos) {
+		ChessPiece p = (ChessPiece) getBoard().piece(pos);
+		return p == null || p.getColor() != this.getColor();
+	}
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getCols()];
+		
+		Position p = new Position(0, 0);
+		// UP
+		p.setValues(pos.getRow() - 1, pos.getCol());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		// UP-RIGHT
+		p.setValues(pos.getRow() - 1, pos.getCol() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		// RIGHT
+		p.setValues(pos.getRow(), pos.getCol() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		// DOWN-RIGHT
+		p.setValues(pos.getRow() + 1, pos.getCol() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		// DOWN
+		p.setValues(pos.getRow() + 1, pos.getCol());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		// DOWN-LEFT
+		p.setValues(pos.getRow() + 1, pos.getCol() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		// LEFT
+		p.setValues(pos.getRow(), pos.getCol() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
+		// LEFT-UP
+		p.setValues(pos.getRow() - 1, pos.getCol() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getCol()] = true;
+		}
+		
 		return mat;
 	}
 }
